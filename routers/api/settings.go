06769bda594f2208b24eca5b5efef2ee3e478604
larmic <mail@larmic.de>
2023-09-03 10:48:26 +0200

    add first rest service

--- routers/api/settings.go
+++ routers/api/settings.go
@@ -11,17 +11,20 @@ type chargerSettingsDto struct {
 	Model        string `json:"model"`
 	HWVersion    string `json:"hardwareVersion"`
 	SWVersion    string `json:"softwareVersion"`
+	Connectors   int    `json:"numberOfConnectors"`
 }
 
 func GetSettings(c *gin.Context) {
 	charger := client.NewClient("10.0.40.200")
 
 	settings := charger.ReadSettings()
+	connectors := charger.ReadNumberOfConnectors()
 
 	c.IndentedJSON(http.StatusOK, chargerSettingsDto{
 		SerialNumber: settings.SerialNumber,
 		Model:        settings.Model,
 		HWVersion:    settings.HWVersion,
 		SWVersion:    settings.SWVersion,
+		Connectors:   connectors,
 	})
 }
